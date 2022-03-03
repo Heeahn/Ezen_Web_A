13fa36ad3902fefa16403fca765f30e19f806162
504 <504@DESKTOP-F07912N>
2022-03-03 13:23:56 +0900

    Day03

--- Java_1day/src/Day03/Day03_3.java
+++ Java_1day/src/Day03/Day03_3.java
@@ -69,12 +69,31 @@ public class Day03_3 { // cs
 				영어점수 100점 이면 '영어우수' 출력
 				수학점수 100점 이면 '수학우수' 출력
 			평균이 80점 이상이면서 
-				국어점수 90점 이면 '국어장려' 출력
-				영어점수 90점 이면 '영어정려' 출력
-				수학점수 90점 이면 '수학장려' 출력
+				국어점수 90점이상 이면 '국어장려' 출력
+				영어점수 90점이상 이면 '영어정려' 출력
+				수학점수 90점이상 이면 '수학장려' 출력
 			그외 재시험 
 	 */
-
+		
+		System.out.println("국어점수 :"); int 국어 = scanner.nextInt();
+		System.out.println("영어점수 :"); int 영어 = scanner.nextInt();
+		System.out.println("수학점수 :"); int 수학 = scanner.nextInt();
+		int 평균 = (국어+영어+수학)/3;
+		
+		if(평균>=90) {
+			if(국어 ==100) System.out.println("국어우수");
+			if(영어 ==100) System.out.println("영어우수");
+			if(수학 ==100) System.out.println("수학우수");
+		}
+		else if(평균>=80) {
+			if(국어 >=90) System.out.println("국어장려");
+			if(영어 >=90) System.out.println("영어장려");
+			if(수학 >=90) System.out.println("수학장려");
+		}
+		else {
+			System.out.println("재시험");
+		}
+				
 	
 		
 		
