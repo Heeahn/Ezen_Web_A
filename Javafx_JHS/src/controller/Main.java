6d244e35e027245fa06b57f9b5af6794f672a10b
Heeahn <100547828+Heeahn@users.noreply.github.com>
2022-04-04 09:42:58 +0900

    ss

--- Javafx_JHS/src/controller/Main.java
+++ Javafx_JHS/src/controller/Main.java
@@ -1,4 +1,4 @@
-package controller.login;
+package controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
--- Javafx_JHS/src/controller/login/Loginpane.java
+++ Javafx_JHS/src/controller/login/Loginpane.java
@@ -3,6 +3,7 @@ package controller.login;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import dao.MemberDao;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -15,7 +16,7 @@ public class Loginpane implements Initializable {
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		
+		lblconfirm.setText(""); // 처음 실행시 문구 숨기기
 	}
 	
     @FXML
@@ -41,39 +42,61 @@ public class Loginpane implements Initializable {
 
     @FXML
     void accfindid(ActionEvent event) {
-    	System.out.println(" 아이디찾기 페이지로 이동 ");
+    	Login.instance.loadpage("/view/login/findidpane.fxml");
     }
 
     @FXML
     void accfindpassword(ActionEvent event) {
-    	System.out.println(" 패스워드찾기 페이지로 이동 ");
+    	Login.instance.loadpage("/view/login/findpasswordpane.fxml");
     }
 
     @FXML
     void accsignup(ActionEvent event) {
-    	System.out.println(" 회원가입 페이지로 이동 ");
     	// *회원가입 버튼을 누르면 login컨트롤클래스 내 borderpane 컨테이너 center 페이지 변경
     	// 문제 : 다른 클래스내 컨테이너 호출 
 //    		Login login = new Login(); // 새로운 컨테이너 생성 [ x ]
 //    		login.loadpage("/view/signuppane.fxml"); // [ x ]
     	// 해결방안 : 기존 컨테이너 가져오는 방법 !!!! -> this
-    	Login.instance.loadpage("/view/view.login/signuppane.fxml");
+    	Login.instance.loadpage("/view/login/signuppane.fxml");
     }
 
     @FXML
     void login(ActionEvent event) {
-    	//    System.out.println(txtid.getText()+"로 아이디가 입력되엇습니다.");
-    	//	System.out.println(txtpassword.getText()+"로 패스워드가 입력되엇습니다.");
+    	//1. 컨트롤[fxid]에 입력된 값 가져오기
     	String id = txtid.getText();
     	String password = txtpassword.getText();
+    	//2. db객체내 메소드 호출 
+    	boolean result = MemberDao.memberDao.login(id, password);
+    	//3. 결과 확인 
+    	if( result ) {
+    		// 페이지 전환 [ 다음시간 ] 
+    		// * 테스트 
+    		lblconfirm.setText("로그인성공");
+    	}else {
+    		lblconfirm.setText("동일한 회원정보가 없습니다.");
+    	}
     	
-    	if(id.equals("admin") && password.equals("1234")) lblconfirm.setText("관리자이군요!!!");
-    	else lblconfirm.setText("일반회원이군요!!!");
-    	System.out.println("로그인 처리");
     }
 	
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
